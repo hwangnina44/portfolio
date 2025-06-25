65f424c8885ad36ca055fa094260167bb5ed4061
hwangnina44 <hwangnina44@gmail.com>
2025-06-25 17:25:01 +0900

    포포루 디

--- script/index.js
+++ script/index.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
     //초기설정
-    $(".pager li:first").css ({
-        backgroundColor : '#333'
+    $(".pager li:first").css({
+        backgroundColor: '#333'
     })
 
-    var auto =setInterval(left, 3000);
+    var auto = setInterval(left, 3000);
 
     $('.mapbox .view .imgbox').mouseenter(function () {
         //자동실행 멈춤, 인터벌 제거 
@@ -12,7 +12,7 @@ $(document).ready(function () {
 
     }).mouseleave(function () {
         //자동실행 다시 시작!, 인터벌 재가동!
-       auto=setInterval(left,3000);
+        auto = setInterval(left, 3000);
     });
 
 
@@ -22,33 +22,60 @@ $(document).ready(function () {
 
 
     var page = 0;
+
+
     function left() {
-        if (page < num - 1) {
-            page++
-            $(".mapbox .view").animate({
-                marginLeft: -width * page
-            }, 800);
+        page++;
 
+        $(".mapbox .view").animate({
+            marginLeft: -width * page
+        }, 800, function () {
 
-        } else if (page === num-1) {
-            $(".mapbox .view").css({
-                marginLeft: 0
+            if (page === num - 1) {
+                $(".mapbox .view").css({
+                    marginLeft: 0
+                });
+                page = 0;
+            }
+
+            $(".pager li").eq(page).css({
+                backgroundColor: "#333"
+            }).siblings().css({
+                backgroundColor: "transparent"
             });
-            page = 0;
-        }
-
-        $('.page li.num'+page).css({
-            backgroundColor :"#333"
-        }).siblings().css({
-            backgroundColor:"transparent"
         });
-        if(page===5){
-            $('.page li').first().css({
-                 backgroundColor :"#333"
-            });
-        }
-       
-    
     }
+
+});
+
+
+$(document).ready(function () {
+
+    //초기설정
+
+     $(".content-box  > div.concept").show();
+
+
+     $(".content-box a.concept").click(function (e) {
+        e.preventDefault();
+       
+    });
+
+
+    $(".content-box a.btn").click(function () {
+        $(this).addClass('on').siblings().removeClass('on');
+    });
+
+    $(".content-box a.concept").click(function (e) {
+        e.preventDefault();
+        $("content-box  > div.concept").fadein().siblings().hide();
+    });
+
+    $(".content-box a.color").click(function (e) {
+        e.preventDefault();
+        $("content-box  > div.color").show().siblings().hide();
+    });
+
+
 
 });
