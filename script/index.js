a5e8d526e2126261e1bd91d6c69d5b7e09c4c78a
hwangnina44 <hwangnina44@gmail.com>
2025-07-02 04:14:03 +0900

    이미지변경

--- script/index.js
+++ script/index.js
@@ -1,114 +1,56 @@
 $(document).ready(function () {
-    //초기설정
-    $(".pager li:first").css({
-        backgroundColor: '#3A98B9'
-    })
 
-    var auto = setInterval(left, 3000);
+    // [슬라이드 설정]
+    var width = $(".mapbox .view .img-box").width();
+    console.log("슬라이드 이미지 너비:", width);
 
-    $('.mapbox .view .imgbox').mouseenter(function () {
-        //자동실행 멈춤, 인터벌 제거 
-        clearInterval(auto);
-
-    }).mouseleave(function () {
-        //자동실행 다시 시작!, 인터벌 재가동!
-        auto = setInterval(left, 3000);
+    $(".mapbox .view").prepend($(".view .img-box").last()).css({
+        marginLeft: -width
     });
 
-
-    var width = $(".mapbox .view .imgbox").width();
-    var num = $(".mapbox .view .imgbox").length;
-    console.log(width, num);
-
-
-    var page = 0;
-
-
-    function left() {
-        page++;
-
+    $('.mapbox .prev-btn').click(function () {
         $(".mapbox .view").animate({
-            marginLeft: -width * page
+            marginLeft: 0
         }, 800, function () {
-
-            if (page === num - 1) {
-                $(".mapbox .view").css({
-                    marginLeft: 0
-                });
-                page = 0;
-            }
-
-            $(".pager li").eq(page).css({
-                backgroundColor: "#3A98B9"
-            }).siblings().css({
-                backgroundColor: "transparent"
+            $(".mapbox .view").prepend($(".view .img-box").last()).css({
+                marginLeft: -width
             });
         });
-    }
+    });
 
-});
+    $('.mapbox .next-btn').click(function () {
+        $(".mapbox .view").animate({
+            marginLeft: -width
+        }, 800, function () {
+            $(".mapbox .view").append($(".view .img-box").first()).css({
+                marginLeft: 0
+            });
+        });
+    });
 
-
-$(document).ready(function () {
-
-    // 버튼 on 클래스 토글
+    // ✅ [디자인 시안 탭 버튼]
     $(".content-box a.btn").click(function () {
         $(this).addClass('on').siblings().removeClass('on');
     });
 
-    
-    $(".content-box .des-group div.con1").fadeIn().siblings().hide();
-
-   
-    $(".content-box a.con1").click(function (e) {
-        e.preventDefault();
-        $(".content-box .des-box div.con1").fadeIn().siblings().fadeOut();
-    });
-    
-    
-    $(".content-box a.con2").click(function (e) {
-        e.preventDefault();
-        $(".content-box .des-box div.con2").fadeIn().siblings().fadeOut();
-    });
-
-    
-    $(".content-box a.con3").click(function (e) {
-        e.preventDefault();
-        $(".content-box .des-box div.con3").fadeIn().siblings().fadeOut();
-    });
-
-    
-});
-
-$(document).ready(function () {
-
-    // 버튼 on 클래스 토글
-    $(".content-box a.btn").click(function () {
-        $(this).addClass('on').siblings().removeClass('on');
-    });
-
-    // 초기 설정: concept 보이기, 나머지는 fadeOut
+    // 초기 상태: concept만 보여주기
     $(".content-box .des-group div.concept").fadeIn().siblings().hide();
 
-    // 컨셉 버튼 클릭
     $(".content-box a.concept").click(function (e) {
         e.preventDefault();
         $(".content-box .des-group div.concept").fadeIn().siblings().fadeOut();
     });
 
-    // 컬러 버튼 클릭
     $(".content-box a.color").click(function (e) {
         e.preventDefault();
         $(".content-box .des-group div.color").fadeIn().siblings().fadeOut();
     });
-    
-    // 폰트 버튼 클릭
+
     $(".content-box a.font").click(function (e) {
         e.preventDefault();
         $(".content-box .des-group div.font").fadeIn().siblings().fadeOut();
     });
 
-    // 디자인 시안 버튼 클릭
     $(".content-box a.design-des").click(function (e) {
         e.preventDefault();
         $(".content-box .des-group div.design-des").fadeIn().siblings().fadeOut();
