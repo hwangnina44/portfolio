b63191c0bbbdf09e5a942a7d24781f4afce408df
hwangnina44 <hwangnina44@gmail.com>
2025-07-11 17:14:19 +0900

    수정

--- script/index.js
+++ script/index.js
@@ -1,34 +1,13 @@
 $(document).ready(function () {
 
-    // [슬라이드 설정]
-    var width = $(".mapbox .view .img-box").width();
-    console.log("슬라이드 이미지 너비:", width);
-
-    $(".mapbox .view").prepend($(".view .img-box").last()).css({
-        marginLeft: -width
+    var swiper = new Swiper(".mySwiper", {
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
     });
 
-    $('.mapbox .prev-btn').click(function () {
-        $(".mapbox .view").animate({
-            marginLeft: 0
-        }, 800, function () {
-            $(".mapbox .view").prepend($(".view .img-box").last()).css({
-                marginLeft: -width
-            });
-        });
-    });
-
-    $('.mapbox .next-btn').click(function () {
-        $(".mapbox .view").animate({
-            marginLeft: -width
-        }, 800, function () {
-            $(".mapbox .view").append($(".view .img-box").first()).css({
-                marginLeft: 0
-            });
-        });
-    });
-
-    // ✅ [디자인 시안 탭 버튼]
+    // 
     $(".content-box a.btn").click(function () {
         $(this).addClass('on').siblings().removeClass('on');
     });
